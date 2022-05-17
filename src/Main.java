3c96c5108c94f89140d4faa8124f1d857f702bff
Dmitrii <chertov.dmitrii@yandex.ru>
2022-05-17 10:43:40 +0300

    Закончил первую часть ДЗ. Обявил все переменные, которые изучали в уроке.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Домашнее задание");
+        byte first = 123;
+        System.out.println("Переменная byte - " + first);
+        short second = 15687;
+        System.out.println("Переменная short - " + second);
+        int third = 1000000001;
+        System.out.println("Переменная int - " + third);
+        long fourth = -1098406546;
+        System.out.println("Переменная long - " + fourth);
+        float fifth = 3.14f;
+        System.out.println("Переменная float - " + fifth);
+        double sixth = 3.1415926535;
+        System.out.println("Переменная double - " + sixth);
     }
 }
